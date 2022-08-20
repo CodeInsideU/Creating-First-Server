9f6d37db5260633e2d556c18b6393c14be169417
Vratik Zade <83591308+CodeInsideU@users.noreply.github.com>
2022-08-20 20:37:40 +0530

    added nodemon

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
-const port = 8081;
+const port = 8081; //local port number
 
 http
-    .createServer((require, response) => {
+    // creating server
+    .createServer((require, response) => {//call back function
         response.writeHead(200, { "Content-Type": "text/html" });
         response.write("<h1>Hello, this is from my server</h1>");
         response.end();
     })
-    .listen(port, () => {
+    // listening from server
+    .listen(port, () => { // call back function
         console.log('Nodejs server started on port ${port}');
     });
