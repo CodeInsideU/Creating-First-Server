aabe2a2c06a39f93f0db0deb5d9a41df383656bb
Vratik Zade <83591308+CodeInsideU@users.noreply.github.com>
2022-09-09 08:42:08 +0530

    first server

--- server.js
+++ server.js
@@ -1,45 +1,17 @@
-const http = require('http');
+const http = require("http");
 
-const port = 8081; //local port number
-
-// http://localhost:8081
-
-const toDoList = ["Complete Node Byte", "Play Cricket"];
+const port = 8081; // local port no
 
 http
-    // creating server
-    .createServer((require, request) => {
-        const { method, url } = require;
-        // console.log(method, url);
-        // request.end();
-        if (url === "/todos") {
-            if (method === "GET") {
-                request.writeHead(200, { "Content-Type": "text/html" });
-                request.write(toDoList.toString());
-            }
-            else if (method === "POST") {
-                let body = "";
-                require.on('error', (err) => {
-                    console.error(err);
-                })
-                    .on('data', (chunk) => {
-                        body += chunk;
-                    })
-                    .on('end', () => {
-                        body = JSON.parse(body);
-                        console.log("data: ", body);
-                    });
-            }
-            else {
-                request.writeHead(501);
-            }
-        }
-        else {
-            request.writeHead(404);
-        }
-        request.end();
+    .createServer((req, res) => {
+        // callback function
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h1>Hello, this is from my server</h1>");
+        res.end();
     })
-    // listening from server
-    .listen(port, () => { // call back function
-        console.log('Nodejs server started on port ${port}');
+    .listen(port, () => {
+        // callback function
+        console.log(`Nodejs server started on port ${port}`);
     });
+
+// http://localhost:8081
